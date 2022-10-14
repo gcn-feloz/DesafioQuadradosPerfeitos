48f93af351b4fe43cfee04c035e9c6b53fc4a19a
gcn-feloz <albertomacielkraemer@gmail.com>
2022-10-14 01:13:56 -0300

    Ultimas alterações

--- src/DesafioQuadradosPerfeitos.java
+++ src/DesafioQuadradosPerfeitos.java
@@ -4,55 +4,59 @@ public class DesafioQuadradosPerfeitos {
 
     public static void main(String[] args) {
         int n = Integer.parseInt(new Scanner(System.in).nextLine());
-        ArrayList<Integer> quadradoPerf = new ArrayList<Integer>();
+        ArrayList<Double> quadradoPerf = new ArrayList<Double>();
 
         int a = 0; // menor numero de quadrados perfeitos.
+
+        Boolean divPor2 = false;
+        Boolean divPor3 = false;
+
+        Double resultado = 0.0;
 
         for (int i = 0; i <= n; i++) {
             if (i * i >= n) {
                 break;
             }
-            quadradoPerf.add(i * i); // Adiciona na lista os Quadrados perfeitos
+            quadradoPerf.add((double) (i * i)); // Adiciona na lista os Quadrados perfeitos
             System.out.println("contagem: " + i + " ²perf.: " + quadradoPerf.get(i));
         }
         System.out.println(quadradoPerf.size());
 
         for (int i = 0; i < (quadradoPerf.size()); i++) {
-            // System.out.println("Verificando: " + i);
 
-            if (n / 2 == quadradoPerf.get(i)) {
+            if (n / 2d == quadradoPerf.get(i)) {
                 System.out.println("é divisivo por 2:");
                 System.out.println(n / 2);
-            } else if (n / 3 == quadradoPerf.get(i)) {
+                divPor2 = true;
+                break;
+            } else if (n / 3d == quadradoPerf.get(i)) {
                 System.out.println("é divisivo por 3:");
-                System.out.println(n / 3;
+                System.out.println(n / 3d);
+                divPor3 = true;
+                break;
             }
-
         }
-
-        // // i--;
-        // // System.out.println("Finalizou contagem, agora vai subtrair");
-        // // System.out.println("n: " + n);
-        // // System.out.println("i: " + i);
-        // // System.out.println("d[i]: " + d[i]);
-        // // System.out.println("Subtraindo " + n + "-" + d[i]);
-        // // n = n-d[i];
-        // // i--;
-        // System.out.println("Valor de i: " + i);
-        // for (int ii = i; ii > 0; ii--) {
-        // System.out.println("--Entrou no FOR--");
-
-        // System.out.println("N é menor ou igual que d[ii]: " + n + ">=" + d[ii] + " :
-        // " + (n >= d[ii]));
-        // if (n >= d[ii]) {
-        // n = n - d[ii];
-        // System.out.println("Valor de n: " + n);
-        // a++;
-        // }
-
-        // }
-        // System.out.println("Menor quantidade de quadrados perfeitos é:" + a);
-
+        if (divPor2) {
+            System.out.println("2");
+        } else if (divPor3) {
+            System.out.println("3");
+        } else {
+            for (int ii = quadradoPerf.size() - 1; ii > 0; ii--) {
+                if (n == 0) {
+                    break;
+                }
+                
+                
+                while (n >= quadradoPerf.get(ii)) {
+                    System.out.println("N é menor ou igual: " + n + ">=" + quadradoPerf.get(ii) + ""
+                        + (n >= quadradoPerf.get(ii)));
+                    n = (int) (n - quadradoPerf.get(ii));
+                    System.out.println("Valor de n: " + n);
+                    a++;
+                }
+            }
+            System.out.println("Menor quantidade de quadrados perfeitos é:" + a);
+        }
     }
 
 }
